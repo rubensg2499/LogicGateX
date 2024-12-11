1806155fcc06697fe906e1292d8edb27a1a6e1b4
Ruben Sanchez Gonzalez <>
2024-12-11 16:08:17 -0600

    Crear Compuertas Logicas con un compilador

---
+++ src/main/java/com/logicgatex/logicgatex/LogicGateX.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.logicgatex.logicgatex;
+
+/**
+ *
+ * @author sango
+ */
+public class LogicGateX {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
